6a09b65231e9a8726103de09ed81dd66c0e47f04
cruzsolano <30808112+cruzsolano@users.noreply.github.com>
2017-08-31 20:41:35 -0700

    scrolling works

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ScrollView: UIScrollView!
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0..<3 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            ScrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 100, y: (view.frame.size.height / 2) - 100, width: 200, height: 200)
+            
+        }
+        
+        ScrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
